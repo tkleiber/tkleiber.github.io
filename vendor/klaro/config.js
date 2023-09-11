ce0f10b01e7e742db76fa2de0d0685750e1939f3
Torsten Kleiber <torsten.kleiber@web.de>
2023-09-11 21:55:56 +0200

    https://github.com/tkleiber/blog.github.io/commit/2ff0a02e832620e54e7c321c445a9f9c3c261957 Merge pull request #155 from tkleiber/150-upgrade-to-hyvor-talk-version-3
    fix #150 switch from hyvor to giscus

--- vendor/klaro/config.js
+++ vendor/klaro/config.js
@@ -13,11 +13,11 @@ translations: {
         description:
             'Here you can see and customize the information that we collect about you. You can change these cookie settings at any time from the footer of each page.',
       },
-      hyvor: {
-        description: 'Visit time and read status of users for comments, see https://talk.hyvor.com/docs/privacy',
+      giscus: {
+        description: 'Login of users for comments, see https://github.com/giscus/giscus/blob/main/PRIVACY-POLICY.md',
       },
       cloudflare: {
-        description: 'Protection against DDoS attacks, see https://support.cloudflare.com/hc/en-us/articles/200170156-Understanding-the-Cloudflare-Cookies#12345682',
+        description: 'Protection against DDoS attacks, https://www.cloudflare.com/privacypolicy',
       },
       purposes: {
         commenting: 'Commenting',
@@ -27,24 +27,10 @@ translations: {
   },
   services: [
     {
-      name: 'hyvor',
-      title: 'Hyvor Talk',
+      name: 'giscus',
+      title: 'giscus',
       purposes: ['commenting'],
-      cookies: [
-        [/^(authsess)|(talksess)$/, '/', 'talk.hyvor.com'], //for the production version
-        [/^(authsess)|(talksess)$/, '/', '127.0.0.1'], //for the local version
-        'hyvor_ignore',
-      ],
-      /*
-      callback: function(consent, app) {
-        console.log(
-            'User consent for app ' + app.name + ': consent=' + consent
-        );
-        if( consent == false ) {
-            // hyvor_talk.reload();
-        }
-      }
-      */
+      required: true,
     },
     {
       name: 'cloudflare',
